e061ad6d3e33bbd8639f8bd436915f89fecd05f7
Evan Pipis <ievangelos@icloud.com>
2024-01-01 01:48:09 +0000

    Taking basic order details • Added 2 new views (OrderView, AddressView) • Added a new Order class • Created a form for the selection of the cupcake types, quantity.

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Evangelos Pipis on 01/01/2024.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    var extraFrosting = false
+    var addSprinkles = false
+}
---
+++ CupcakeCorner/OrderView.swift
@@ -0,0 +1,49 @@
+//
+//  OrderView.swift
+//  CupcakeCorner
+//
+//  Created by Evangelos Pipis on 01/01/2024.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @State private var order = Order()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) { selection in
+                            Text(Order.types[selection])
+                        }
+                    }
+                    
+                    Stepper("Number of cupcakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery Details") {
+                        AddressView(order: order)
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+}
